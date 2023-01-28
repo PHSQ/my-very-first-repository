5490e38e5d4999a6f5dfc611a18b16266b0aa8c1
Pedro Queiroz <phsq@poli.br>
2023-01-28 17:12:01 -0300

    ex1 compreendido

---
+++ python-essential-training/ex1.py
@@ -0,0 +1,9 @@
+from termcolor import colored
+
+#ex1 1 entendido completamente
+print(colored('~~~RAINBOW~~~', 'red'))
+print(colored('~~~RAINBOW~~~', 'yellow'))
+print(colored('~~~RAINBOW~~~', 'green'))
+print(colored('~~~RAINBOW~~~', 'cyan'))
+print(colored('~~~RAINBOW~~~', 'blue'))
+print(colored('~~~RAINBOW~~~', 'magenta'))
