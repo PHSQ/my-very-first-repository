adfeaacde066efde2acb431869e2664fd83bf2f9
Pedro Queiroz <phsq@poli.br>
2023-01-29 22:33:39 -0300

    Created: A guessing game

---
+++ programming-foudations/guessing_game.py
@@ -0,0 +1,8 @@
+mff = input("What's my favorite food? ") #mff stands for my favorite food
+
+if mff == "Sushi":
+    print("Oh yeah, you right!")
+else:
+    print("Naaah")
+
+print("Thanks for playing!")
