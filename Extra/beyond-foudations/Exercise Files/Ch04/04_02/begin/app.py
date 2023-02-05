68ea68646215ff6a8ed89060c624e68e87c9f693
Pedro Queiroz <phsq@poli.br>
2023-02-05 13:02:57 -0300

    atualizaçõa

--- Extra/beyond-foudations/Exercise Files/Ch04/04_02/begin/app.py
+++ Extra/beyond-foudations/Exercise Files/Ch04/04_02/begin/app.py
@@ -1,3 +1,11 @@
 first_name = 'malala'
 last_name = 'yousafzai'
 note = 'award: Nobel Peace Prize'
+
+print(first_name.capitalize())
+print(last_name.capitalize())
+
+award_location = note.find('award: ')
+print(award_location)
+
+print(note[7:])
